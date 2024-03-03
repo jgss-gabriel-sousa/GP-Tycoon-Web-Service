5d9a324a43740b2e46fc21f7c654ff8ddceb3bff
Gabriel Sousa <jgss.gabriel.sousa@gmail.com>
2024-03-02 21:50:00 -0300

    Update server.js

--- server.js
+++ server.js
@@ -12,8 +12,13 @@ const cors = require('cors');
 const fs = require("fs");
 const path = require("path");
 
-app.use(cors());
 app.use(express.json());
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
+    app.use(cors());
+    next();
+});
 
 //######################################################## GP TYCOON ######################
 
